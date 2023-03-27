ded4d26f5c84e17f9edd34ef495fa412d969a30d
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-03-28 05:34:24 +0600

    single product display

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,9 +1,10 @@
 const express = require("express")
 const router = express.Router();
 
-const { getProducts, newProduct } = require("../controllers/productControllers");
+const { getProducts, newProduct, getSingleProduct } = require("../controllers/productControllers");
 
 router.route("/products").get(getProducts);
+router.route("/product/:id").get(getSingleProduct);
 
 router.route("/product/new").post(newProduct);
 
