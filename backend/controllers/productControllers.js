28fbc87af1faaaacbb9bc43cbc2975dc4a254eea
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-03-28 19:36:42 +0600

    update products

--- backend/controllers/productControllers.js
+++ backend/controllers/productControllers.js
@@ -40,3 +40,27 @@ exports.getSingleProduct = async (req, res, next) => {
         product
     })
 }
+
+// Update Product   =>  /api/v1/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+
+    let product = await Product.findById(req.params.id);
+
+    if (!product){
+        return res.status(404).json({
+            success: false,
+            message: "Products not found"
+        })
+    }
+    
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidator: true
+    });
+
+    res.status(200).json({
+        success: true,
+        product
+    })
+}
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -1,11 +1,14 @@
 const express = require("express")
 const router = express.Router();
 
-const { getProducts, newProduct, getSingleProduct } = require("../controllers/productControllers");
+const { getProducts, newProduct, getSingleProduct, updateProduct } = require("../controllers/productControllers");
 
 router.route("/products").get(getProducts);
+
 router.route("/product/:id").get(getSingleProduct);
 
-router.route("/product/new").post(newProduct);
+router.route("/admin/product/new").post(newProduct);
+
+router.route("/admin/product/:id").put(updateProduct);
 
 module.exports = router;
