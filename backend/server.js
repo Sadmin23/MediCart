17894372f6b989a8bc6432185573228f12573a94
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-10 22:55:25 +0600

    uncaught exceptions

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const connectDatabase = require('./config/database')
 
 const dotenv = require("dotenv");
 
+process.on('uncaughtException', err => {
+    console.log(`ERROR: ${err.stack}`);
+    console.log('Shutting down server due to uncaught exception');
+    process.exit(1);
+})
+
 //setting up config file
 dotenv.config({path: "backend/config/config.env"});
 
@@ -15,7 +21,7 @@ const server = app.listen(process.env.PORT, () => {
 
 // Handle Unhandled 
 process.on('unhandledRejection', err => {
-    console.log('ERROR: ${err.message}');
+    console.log(`ERROR: ${err.message}`);
     console.log('Shutting down the server due to Unhandled Promise rejection');
     server.close(() => {
         process.exit(1)
