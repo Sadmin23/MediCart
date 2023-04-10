ce70ca0fb1527bdd48b483dbf4c2d0d4fea20326
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-10 19:51:56 +0600

    errors middleware

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+const errorMiddleware = require('./middleware/errors')
+
 app.use(express.json());
 
 // Importing all routes
@@ -8,5 +10,8 @@ app.use(express.json());
 const products = require("./routes/products");
 
 app.use("/api/v1", products);
+
+//Middleware to handle errors
+app.use(errorMiddleware);
 
 module.exports = app;
