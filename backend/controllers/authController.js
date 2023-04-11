ffa2502bf0b9049efbb9ff0d5c6adc2eafbdf8b9
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-12 05:07:34 +0600

    password encrypted

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -5,13 +5,7 @@ const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
 
 // Register a user   => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
-/*
-    const result = await cloudinary.v2.uploader.upload(req.body.avatar, {
-        folder: 'avatars',
-        width: 150,
-        crop: "scale"
-    })
-*/
+
     const { name, email, password } = req.body;
 
     const user = await User.create({
@@ -23,8 +17,6 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
             url: 'https://static.vecteezy.com/system/resources/thumbnails/002/002/403/small/man-with-beard-avatar-character-isolated-icon-free-vector.jpg'
         }
     })
-
-//    sendToken(user, 200, res)
 
     res.status(201).json({
         success: true,
--- backend/models/user.js
+++ backend/models/user.js
@@ -45,4 +45,13 @@ const userSchema = new mongoose.Schema({
 
 })
 
+// Encrypting password before saving user
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) {
+        next()
+    }
+
+    this.password = await bcrypt.hash(this.password, 10)
+})
+
 module.exports = mongoose.model('User', userSchema);
