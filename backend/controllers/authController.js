4f5a2d6cc84783d23cbbe8a84d6c5d051a9ea495
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-12 13:10:42 +0600

    cookies used

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 
 const ErrorHandler = require('../utils/errorHandler');
 const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+const sendToken = require('../utils/jwtToken');
 
 // Register a user   => /api/v1/register
 exports.registerUser = catchAsyncErrors(async (req, res, next) => {
@@ -18,12 +19,7 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
         }
     })
 
-    const token = user.getJwtToken();
-
-    res.status(201).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
 
 })
 
@@ -52,8 +48,5 @@ exports.loginUser = catchAsyncErrors(async (req, res, next) => {
 
     const token = user.getJwtToken();
 
-    res.status(201).json({
-        success: true,
-        token
-    })
+    sendToken(user, 200, res)
 })
