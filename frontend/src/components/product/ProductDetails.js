21b7066553a6faf40472c1553b5f3d6a92529f31
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-19 20:21:23 +0600

    product details added

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -1,4 +1,5 @@
 import React, { Fragment, useEffect } from 'react'
+import { Carousel } from 'react-bootstrap'
 
 import Loader from '../layout/Loader'
 import MetaData from '../layout/MetaData'
@@ -13,8 +14,6 @@ const ProductDetails = ({match}) => {
 
     const dispatch = useDispatch();
     const alert = useAlert();
-
-    const params = useParams();
 
     const { loading, error, product } = useSelector(state => state.productDetails)
 
@@ -37,19 +36,25 @@ const ProductDetails = ({match}) => {
             <Fragment>
                         <div className="row f-flex justify-content-around">
             <div className="col-12 col-lg-5 img-fluid" id="product_image">
-                <img src="https://i5.walmartimages.com/asr/1223a935-2a61-480a-95a1-21904ff8986c_1.17fa3d7870e3d9b1248da7b1144787f5.jpeg?odnWidth=undefined&odnHeight=undefined&odnBg=ffffff" alt="sdf" height="500" width="500"/>
+                            <Carousel pause='hover'>
+                                {product.images && product.images.map(image => (
+                                    <Carousel.Item key={image.public_id}>
+                                        <img className="d-block w-100" src={image.url} alt={product.title} />
+                                    </Carousel.Item>
+                                ))}
+                            </Carousel>
             </div>
 
             <div className="col-12 col-lg-5 mt-5">
-                <h3>onn. 32” className HD (720P) LED Roku Smart TV (100012589)</h3>
-                <p id="product_id">Product # sklfjdk35fsdf5090</p>
+                <h3>{product.name}</h3>
+                <p id="product_id">Product # {product._id}</p>
 
                 <hr/>
 
                 <div className="rating-outer">
-                    <div className="rating-inner"></div>
+                    <div className="rating-inner" style={{ width: `${(product.ratings / 5) * 100}%` }}></div>
                 </div>
-                <span id="no_of_reviews">(5 Reviews)</span>
+                <span id="no_of_reviews">({product.numOfReviews} Reviews)</span>
 
                 <hr/>
 
@@ -65,12 +70,12 @@ const ProductDetails = ({match}) => {
 
                 <hr/>
 
-                <p>Status: <span id="stock_status">In Stock</span></p>
+                <p>Status: <span id="stock_status" className={product.stock > 0 ? 'greenColor' : 'redColor'} >{product.stock > 0 ? 'In Stock' : 'Out of Stock'}</span></p>
 
                 <hr/>
 
                 <h4 className="mt-2">Description:</h4>
-                <p>Binge on movies and TV episodes, news, sports, music and more! We insisted on 720p High Definition for this 32" LED TV, bringing out more lifelike color, texture and detail. We also partnered with Roku to bring you the best possible content with thousands of channels to choose from, conveniently presented through your own custom home screen.</p>
+                <p>{product.description}</p>
                 <hr/>
                 <p id="product_seller mb-3">Sold by: <strong>Amazon</strong></p>
 				
