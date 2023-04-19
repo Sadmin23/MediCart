e0d5f516a709d5c571b90801c8e0fe45ba7d5663
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-19 20:06:21 +0600

    bug fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/layout/Header'
 import Footer from './components/layout/Footer'
 
 import Home from './components/Home';
+import ProductDetails from './components/product/ProductDetails'
 
 function App() {
     return (
@@ -13,6 +14,7 @@ function App() {
                     <div className='container container-fluid'>
                         <Routes>
                             <Route path='/' element={<Home/>} />
+                            <Route path="/product/:id" element={<ProductDetails/>} exact />
                         </Routes>
                     </div>
                 <Footer/>
