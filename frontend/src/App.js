7d76c53ba30d3b8a522fe871a49af39b692847c8
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-21 15:33:16 +0600

    forgot password

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,6 +12,7 @@ import Register from './components/user/Register'
 import Profile from './components/user/Profile'
 import UpdateProfile from './components/user/UpdateProfile'
 import UpdatePassword from './components/user/UpdatePassword'
+import ForgotPassword from './components/user/ForgotPassword'
 
 import ProtectedRoute from './components/route/ProtectedRoute'
 import { loadUser } from './actions/userActions'
@@ -31,8 +32,10 @@ function App() {
                         <Route path="/" component={Home} exact />
                         <Route path="/search/:keyword" component={Home} />
                         <Route path="/product/:id" component={ProductDetails} exact />
+                        
                         <Route path="/login" component={Login} />
                         <Route path="/register" component={Register} />
+                        <Route path="/password/forgot" component={ForgotPassword} exact />
                         <ProtectedRoute path="/me" component={Profile} exact />
                         <ProtectedRoute path="/me/update" component={UpdateProfile} exact />
                         <ProtectedRoute path="/password/update" component={UpdatePassword} exact />
--- frontend/src/actions/userActions.js
+++ frontend/src/actions/userActions.js
@@ -16,6 +16,9 @@ import {
     UPDATE_PASSWORD_REQUEST,
     UPDATE_PASSWORD_SUCCESS,
     UPDATE_PASSWORD_FAIL,
+    FORGOT_PASSWORD_REQUEST,
+    FORGOT_PASSWORD_SUCCESS,
+    FORGOT_PASSWORD_FAIL,
     LOGOUT_SUCCESS,
     LOGOUT_FAIL,
     CLEAR_ERRORS
@@ -163,6 +166,33 @@ export const logout = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: LOGOUT_FAIL,
+            payload: error.response.data.message
+        })
+    }
+}
+
+// Forgot password
+export const forgotPassword = (email) => async (dispatch) => {
+    try {
+
+        dispatch({ type: FORGOT_PASSWORD_REQUEST })
+
+        const config = {
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }
+
+        const { data } = await axios.post('/api/v1/password/forgot', email, config)
+
+        dispatch({
+            type: FORGOT_PASSWORD_SUCCESS,
+            payload: data.message
+        })
+
+    } catch (error) {
+        dispatch({
+            type: FORGOT_PASSWORD_FAIL,
             payload: error.response.data.message
         })
     }
