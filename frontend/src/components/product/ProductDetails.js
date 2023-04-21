3b76fcc633a8e9dc4555164d12c73de119f5b658
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-21 23:08:03 +0600

    quantity selection

--- frontend/src/components/product/ProductDetails.js
+++ frontend/src/components/product/ProductDetails.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useEffect } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { Carousel } from 'react-bootstrap'
 
 import Loader from '../layout/Loader'
@@ -11,6 +11,8 @@ import { getProductDetails, clearErrors } from '../../actions/productActions'
 import { useParams } from 'react-router-dom'
 
 const ProductDetails = ({match}) => {
+
+    const [quantity, setQuantity] = useState(1)
 
     const dispatch = useDispatch();
     const alert = useAlert();
@@ -28,6 +30,25 @@ const ProductDetails = ({match}) => {
       }
     }, [dispatch, alert, error, id]);
 
+    const increaseQty = () => {
+        const count = document.querySelector('.count')
+
+        if (count.valueAsNumber >= product.stock) return;
+
+        const qty = count.valueAsNumber + 1;
+        setQuantity(qty)
+    }
+
+    const decreaseQty = () => {
+
+        const count = document.querySelector('.count')
+
+        if (count.valueAsNumber <= 1) return;
+
+        const qty = count.valueAsNumber - 1;
+        setQuantity(qty)
+
+    }
 
   return (
 
@@ -61,11 +82,11 @@ const ProductDetails = ({match}) => {
 
                 <p id="product_price">$108.00</p>
                 <div className="stockCounter d-inline">
-                    <span className="btn btn-danger minus">-</span>
+                    <span className="btn btn-danger minus" onClick={decreaseQty}>-</span>
 
-                    <input type="number" className="form-control count d-inline" value="1" readOnly />
+                    <input type="number" className="form-control count d-inline" value={quantity} readOnly />
 
-                    <span className="btn btn-primary plus">+</span>
+                    <span className="btn btn-primary plus" onClick={increaseQty}>+</span>
                 </div>
                  <button type="button" id="cart_btn" className="btn btn-primary d-inline ml-4">Add to Cart</button>
 
