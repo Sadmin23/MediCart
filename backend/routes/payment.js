cc74b4de2fcd1bf8b9d4db6c2ddbc215cd19a071
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-22 19:57:29 +0600

    stripe frontend

--- backend/routes/payment.js
+++ backend/routes/payment.js
@@ -2,11 +2,13 @@ const express = require('express')
 const router = express.Router();
 
 const {
-    processPayment
+    processPayment,
+    sendStripApi
 } = require('../controllers/paymentController')
 
 const { isAuthenticatedUser } = require('../middlewares/auth')
 
 router.route('/payment/process').post(isAuthenticatedUser, processPayment);
+router.route('/stripeapi').get(isAuthenticatedUser, sendStripApi);
 
 module.exports = router;
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 
 import Header from './components/layout/Header'
@@ -24,12 +24,24 @@ import NewPassword from './components/user/NewPassword'
 import ProtectedRoute from './components/route/ProtectedRoute'
 import { loadUser } from './actions/userActions'
 import store from './store'
+import axios from 'axios'
 
 function App() {
 
+    const [stripeApiKey, setStripeApiKey] = useState('');
+
     useEffect(() => {
-        store.dispatch(loadUser())    
-      }, [])
+      store.dispatch(loadUser())
+  
+      async function getStripApiKey() {
+        const { data } = await axios.get('/api/v1/stripeapi');
+  
+        setStripeApiKey(data.stripeApiKey)
+      }
+  
+      getStripApiKey();
+  
+    }, [])
 
     return (
         <Router>
