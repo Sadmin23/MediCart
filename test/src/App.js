a94bd92653df219e18355417df923f4caae1e0a9
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-23 22:19:39 +0600

    UI tested

---
+++ test/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import Hero from './Hero';
+import Footer from './Footer';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Hero />
+      <Footer />
+    </div>
+  );
+}
+
+
+export default App;
