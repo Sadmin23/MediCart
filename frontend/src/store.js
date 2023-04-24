ecbc416ec5aa0d24bcaded6311e4eb96b2579499
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-24 16:24:14 +0600

    product deletion

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,7 +2,7 @@ import { createStore, combineReducers, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
-import { productsReducer, newProductReducer, productDetailsReducer, newReviewReducer} from './reducers/productReducers'
+import { productsReducer, newProductReducer, productReducer, productDetailsReducer, newReviewReducer} from './reducers/productReducers'
 import { authReducer, userReducer, forgotPasswordReducer } from './reducers/userReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { newOrderReducer, myOrdersReducer, orderDetailsReducer } from './reducers/orderReducers'
@@ -12,6 +12,7 @@ const reducer = combineReducers({
     products: productsReducer,
     productDetails: productDetailsReducer,
     newProduct: newProductReducer,
+    product: productReducer,
     auth: authReducer,
     user: userReducer,
     forgotPassword: forgotPasswordReducer,
