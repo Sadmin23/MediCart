2c5aae82d3de20aa970c83afb95d2496db392416
Sadmin-Tahmid <sadmid26tahmid@gmail.com>
2023-04-24 20:03:57 +0600

    admin updates user

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension'
 
 import { productsReducer, newProductReducer, productReducer, productDetailsReducer, newReviewReducer} from './reducers/productReducers'
-import { authReducer, userReducer, forgotPasswordReducer, allUsersReducer } from './reducers/userReducers'
+import { authReducer, userReducer, forgotPasswordReducer, allUsersReducer, userDetailsReducer } from './reducers/userReducers'
 import { cartReducer } from './reducers/cartReducers'
 import { newOrderReducer, myOrdersReducer, orderDetailsReducer, allOrdersReducer, orderReducer } from './reducers/orderReducers'
 
@@ -16,6 +16,7 @@ const reducer = combineReducers({
     auth: authReducer,
     user: userReducer,
     allUsers: allUsersReducer,
+    userDetails: userDetailsReducer,
     forgotPassword: forgotPasswordReducer,
     cart: cartReducer,
     newOrder: newOrderReducer,
